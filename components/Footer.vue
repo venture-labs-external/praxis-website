7bfaf5eb452e5a3d33018be55da8acba98ef8b30
Monika Jędrzejak <jedrzejak.monika@tlen.pl>
2022-11-17 10:52:38 +0100

    change styles for desktop

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,22 @@
 <template>
   <footer
-    class="footer dark-green white--text d-flex flex-column align-center py-8"
+    class="footer dark-green white--text d-flex flex-column flex-md-row justify-md-space-between align-center py-8 px-md-10"
   >
-    <div class="d-flex flex-column align-center">
-      <img src="/map-pin.svg" alt="map pin" class="mb-4" />
+    <div
+      class="d-flex flex-column align-center align-md-start text-center text-md-left"
+    >
+      <img src="/map-pin-white.svg" alt="map pin" class="white--text mb-4" />
       <div>{{ contactData.address }}</div>
-      <div class="text-center">{{ contactData.doctors }}</div>
-      <img src="/clock.svg" alt="clock" class="my-4" />
+      <div>{{ contactData.doctors }}</div>
+    </div>
+    <div
+      class="d-flex flex-column align-center align-md-start text-center text-md-left"
+    >
+      <img
+        src="/clock-white.svg"
+        alt="clock"
+        class="white--text my-4 my-md-0 mb-md-4"
+      />
       <div>
         <div v-for="(time, index) in contactData.workingTime" :key="index">
           <div>{{ time.day }} {{ time.hours }}</div>
@@ -16,20 +26,22 @@
       <div>{{ $t('homepage.telephone') }} {{ contactData.phone }}</div>
       <div>{{ $t('homepage.email') }} {{ contactData.mail }}</div>
     </div>
-    <div v-show="$vuetify.breakpoint.mdAndUp" class="footer__logo">
-      <a
-        href="/"
-        class="d-flex flex-column justify-center align-center text-decoration-none"
-      >
-        <img src="/logo-white.svg" alt="Logo" />
-        <span class="flex-wrap text-center white--text">
-          Frauenärztinnen Gerresheim
-        </span>
-      </a>
-    </div>
-    <div class="d-flex mt-6">
-      <a href="/" class="white--text">{{ $t('homepage.imprint') }} </a>
-      <a href="/" class="white--text"> {{ $t('homepage.privacy') }}</a>
+    <div class="d-md-flex flex-md-column align-md-end align-self-sm-end">
+      <div v-show="$vuetify.breakpoint.mdAndUp" class="footer__logo">
+        <a
+          href="/"
+          class="d-flex flex-column justify-center align-center text-decoration-none"
+        >
+          <img src="/logo-white.svg" alt="Logo" />
+          <span class="flex-wrap text-center white--text">
+            Frauenärztinnen Gerresheim
+          </span>
+        </a>
+      </div>
+      <div class="d-flex mt-6 mt-md-4">
+        <a href="/" class="white--text">{{ $t('homepage.imprint') }} </a>
+        <a href="/" class="white--text"> {{ $t('homepage.privacy') }}</a>
+      </div>
     </div>
   </footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,6 @@
 <template>
   <div>
     <Header />
-    <!-- TODO: delete this lorem ipsum after test -->
-    <div class="pt-16">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit error
-      praesentium blanditiis voluptatum quisquam nam fuga ducimus aliquam,
-      ipsam, nisi, earum assumenda laboriosam ad accusamus perspiciatis
-      asperiores nemo cum. Ipsam.
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit error
-      praesentium blanditiis voluptatum quisquam nam fuga ducimus aliquam,
-      ipsam, nisi, earum assumenda laboriosam ad accusamus perspiciatis
-      asperiores nemo cum. Ipsam.
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit error
-      praesentium blanditiis voluptatum quisquam nam fuga ducimus aliquam,
-      ipsam, nisi, earum assumenda laboriosam ad accusamus perspiciatis
-      asperiores nemo cum. Ipsam.
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit error
-      praesentium blanditiis voluptatum quisquam nam fuga ducimus aliquam,
-      ipsam, nisi, earum assumenda laboriosam ad accusamus perspiciatis
-      asperiores nemo cum. Ipsam.
-    </div>
   </div>
 </template>
 
