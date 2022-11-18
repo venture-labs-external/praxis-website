bc1a491038229bb5d85b6d72bbed93dce51fd768
Monika Jędrzejak <jedrzejak.monika@tlen.pl>
2022-11-18 09:06:04 +0100

    refactor code

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -18,13 +18,46 @@
 
 <script>
 import ExpandingCard from '~/components/ExpandingCard';
-import doctors from '~/mixins/aboutUsData.js';
 export default {
   name: 'AboutUs',
   components: {
     ExpandingCard,
   },
-  mixins: [doctors],
+  computed: {
+    // TODO: add missing content
+    doctors() {
+      return [
+        {
+          name: 'Dr. med. Heike Weydandtr',
+          photo: 'dr-weydandtr.png',
+          title: this.$t('homepage.specialistInGynecologyAndObstetrics'),
+          description: this.$t('homepage.afterTheExtensiveTraining'),
+          qualifications: ['qualifikation', 'qualifikation'],
+        },
+        {
+          name: 'Dr. med. R. Korbmacher',
+          photo: 'dr-korbmacher.png',
+          title: this.$t('homepage.specialistInGynecologyAndObstetrics'),
+          description: this.$t('homepage.afterTheExtensiveTraining'),
+          qualifications: [
+            'qualifikation',
+            'qualifikation',
+            'qualifikation',
+            'qualifikation',
+            'qualifikation',
+            'qualifikation',
+          ],
+        },
+        {
+          name: 'Christine Engberg',
+          photo: 'dr-engberg.png',
+          title: this.$t('homepage.specialistInGynecologyAndObstetrics'),
+          description: this.$t('homepage.afterTheExtensiveTraining'),
+          qualifications: ['qualifikation', 'qualifikation'],
+        },
+      ];
+    },
+  },
 };
 </script>
 
