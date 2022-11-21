57ece7538b87b9bed7660d51094bef4e6b5e0f55
Monika Jędrzejak <jedrzejak.monika@tlen.pl>
2022-11-21 11:03:35 +0100

    add slider for mobile

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="contact">
+  <div class="contact py-10">
     <div class="contact__title">
       <span class="subtitle-1 dark-green--text">{{
         $t('homepage.location')
--- components/FlipCard.vue
+++ components/FlipCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="flip-card mx-auto mb-4 mb-md-0"
+    class="flip-card mx-auto mr-4 mb-md-0"
     :class="show ? 'flip-card--click' : ''"
   >
-    <div class="flip-card--inner" @click="show = !show">
-      <div class="flip-card--front">
+    <div class="flip-card--inner">
+      <div class="flip-card--front" @click="show = !show">
         <div v-show="!show">
           <div class="flip-card__icon mint-blue mx-auto">
             <img :src="cardInfo.iconName" class="pa-5" />
@@ -19,12 +19,12 @@
       </div>
       <div class="flip-card--back">
         <div v-show="show">
-          <div class="d-flex flex-row-reverse">
+          <div class="d-flex flex-row-reverse" @click="show = !show">
             <v-btn icon>
               <img src="/cross.svg" />
             </v-btn>
           </div>
-          <div class="text-left mx-6">
+          <div class="text-left mx-6" @click="show = !show">
             <h5 class="text-h5 mb-4">{{ cardInfo.title }}</h5>
             <p class="subtitle-2">
               {{ cardInfo.description }}
