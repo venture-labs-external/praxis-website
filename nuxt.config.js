cd2dc52284635cccbd74bc0553ed9e67b3491159
Monika Jędrzejak <jedrzejak.monika@tlen.pl>
2023-01-05 20:12:56 +0100

    add meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,51 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'author', name: 'author', content: 'Venture Labs' },
+      { hid: 'og:url', name: 'og:url', content: '' },
+      { hid: 'og:image', property: 'og:image', content: '/featured-image.jpg' },
+      {
+        hid: 'title',
+        name: 'title',
+        content: 'Frauenärztinnen Gerresheim',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'featured-image.jpg',
+      },
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'Frauenärztinnen Gerresheim',
+      },
+      {
+        hid: 'apple-mobile-web-app-title',
+        name: 'apple-mobile-web-app-title',
+        content: 'Frauenärztinnen Gerresheim',
+      },
+      {
+        hid: 'og:site_name',
+        name: 'og:site_name',
+        content: 'Frauenärztinnen Gerresheim',
+      },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Frauenärztinnen Gerresheim',
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Frauenärztinnen Gerresheim',
+      },
+      {
+        hid: 'og:favicon',
+        rel: 'icon',
+        type: 'image/x-icon',
+        name: 'og:favicon',
+        content: '/favicon.ico',
+        href: '/favicon.ico',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/praxis.ico' }],
