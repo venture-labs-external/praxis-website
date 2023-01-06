6429f36494dbda4e43943132ec4c17243776ae53
Monika Jędrzejak <jedrzejak.monika@tlen.pl>
2023-01-06 16:19:20 +0100

    add imprint

---
+++ locales/de/imprint.js
@@ -0,0 +1,45 @@
+export default {
+  imprint: 'Impressum',
+  information:
+    'Angaben gemäß § 5 TMG' +
+    '\n' +
+    'Max Muster\n' +
+    'Musterweg\n' +
+    '12345 Musterstadt\n' +
+    '\n' +
+    'Vertreten durch:\n' +
+    'Max Muster\n' +
+    '\n' +
+    'Kontakt:\n' +
+    'Telefon: 01234-789456\n' +
+    'Fax: 1234-56789\n' +
+    'E-Mail: max@muster.de\n' +
+    '\n' +
+    'Umsatzsteuer-ID:\n' +
+    'Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: Musterustid.\n' +
+    '\n' +
+    'Wirtschafts-ID:\n' +
+    'Musterwirtschaftsid\n' +
+    'Aufsichtsbehörde:\n' +
+    'Musteraufsicht Musterstadt\n' +
+    '\n' +
+    'Berufsbezeichnung:\n' +
+    'Ärzte\n' +
+    'Zuständige Kammer: Ärztekammer Nordrhein\n' +
+    'Verliehen durch: Bundesrepublik Deutschland\n' +
+    'Es gelten folgende berufsrechtliche Regelungen: Arzt-Gesetzbuch Regelungen einsehbar unter: http://www.aerztekammer-nrw.de\n' +
+    '\n' +
+    'Haftungsausschluss:' +
+    '\n' +
+    'Haftung für Inhalte\n' +
+    '\n' +
+    'Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.\n' +
+    '\n' +
+    'Datenschutz\n' +
+    '\n' +
+    'Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.\n' +
+    'Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.\n' +
+    'Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.\n' +
+    '\n' +
+    'Website Impressum von impressum-generator.de',
+};
--- locales/en/index.js
+++ locales/en/index.js
@@ -1,5 +1,6 @@
 import homepage from './homepage';
+import imprint from './imprint';
 import labels from './labels';
 import metaTags from './metaTags';
 
-export default { labels, homepage, metaTags };
+export default { labels, imprint, homepage, metaTags };
