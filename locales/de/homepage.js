132efe296e2dec6d76fb97c46d36b5f11e1310cb
Łukasz Kopyto <lukasz.kopyto@gmail.com>
2026-01-08 13:06:36 +0100

    Remove BabyIcon component and related maternity leave messages from German and English locales in the homepage.

--- locales/de/homepage.js
+++ locales/de/homepage.js
@@ -73,7 +73,4 @@ export default {
   weAreWorking:
     'Wir arbeiten aber daran, diesen Service so schnell wie möglich anbieten zu können!',
   team: 'Das Praxisteam',
-  maternityLeave1: 'Liebe Patientinnen,',
-  maternityLeave2:
-    'Frau Dr. med. Rahel Korbmacher ist seit dem <strong>01.Juli</strong> zurück aus dem Mutterschutz. Vereinzelt können die <strong>Öffnungszeiten</strong> jedoch noch von den gewohnten Sprechstundenzeiten abweichen. Donnerstag Nachmittag bleibt die Praxis leider zunächst geschlossen.',
 };
